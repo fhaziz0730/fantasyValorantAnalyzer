e17fcf86e16ac430fdb5b280cd6285526f828087
fhaziz0730 <fhaziz073@gmail.com>
2026-05-18 15:26:32 -0400

    Added historical logs to players that store information about kill, map, and bonus points for game weeks 2-4. Excluded game weeks 1 and 5 due to Americas late start. It will be better for the data to only track weeks where all the regions played at the same time.

--- statsRetrieval/readWebsite.js
+++ statsRetrieval/readWebsite.js
@@ -1,8 +1,13 @@
 import puppeteer from "puppeteer";
 
-function transformRawStringsToPlayers(rawPlayerStrings, rawTeamStrings) {
+function transformRawStringsToPlayers(
+  rawPlayerStrings,
+  rawTeamStrings,
+  rawHistoricalLogStrings,
+) {
   let players = [];
   let index = 0;
+  let index2 = 0;
   for (let rawPlayerString of rawPlayerStrings) {
     //get name from raw player string by slicing it out
     let name = rawPlayerString.slice(0, rawPlayerString.indexOf("\n\t\n\t\n"));
@@ -18,16 +23,32 @@ function transformRawStringsToPlayers(rawPlayerStrings, rawTeamStrings) {
     );
     //get the individual stats from the rest of the raw string
     let values = rawPlayerStringOnlyStats.split("\t");
+    let historicalLog = rawHistoricalLogStrings[index2]
+      .split("\n")
+      .map(function (ln) {
+        return ln.split("\t");
+      });
+    historicalLog = historicalLog.slice(1, 4);
+    historicalLog[0] = historicalLog[0].slice(1);
+    historicalLog[1] = historicalLog[1].slice(1);
+    historicalLog[2] = historicalLog[2].slice(1);
+    for (let i = 0; i < historicalLog.length; i++) {
+      for (let j = 0; j < historicalLog[0].length; j++) {
+        historicalLog[i][j] = Number(historicalLog[i][j]);
+      }
+    }
     players.push({
       name: name,
       role: role,
-      price: values[0],
-      gw_pts: values[1],
-      tot_pts: values[2],
-      ppg: values[3],
+      price: Number(values[0]),
+      gw_pts: Number(values[1]),
+      tot_pts: Number(values[2]),
+      ppg: Number(values[3]),
       team: rawTeamStrings[index],
+      historicalLog: historicalLog,
     });
     index = index + 2;
+    index2 = index2 + 1;
   }
   return players;
 }
@@ -50,25 +71,51 @@ async function getRawTeamStrings(page) {
   }
   return rawTeamStrings;
 }
-
+async function getHistoricalLogStrings(page, numPlayers) {
+  let rawHistoricalLogStrings = [];
+  for (let i = 1; i < numPlayers + 1; i++) {
+    await page.waitForSelector(`tr.group:nth-child(${i})`);
+    await page.click(`tr.group:nth-child(${i})`);
+    await page.waitForSelector(
+      `table.w-full:nth-child(2) > tbody:nth-child(2)`,
+    );
+    let rawHistoricalLogElement = await page.$(
+      `table.w-full:nth-child(2) > tbody:nth-child(2)`,
+    );
+    rawHistoricalLogStrings.push(
+      await page.evaluate((el) => el.innerText, rawHistoricalLogElement),
+    );
+    await page.click("button.absolute");
+  }
+  return rawHistoricalLogStrings;
+}
 async function main() {
   //open a headless chrome browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   //go to stats page and wait for table to load up
-  await page.goto("https://www.valorantfantasyleague.net/playerstats");
+  await page.goto("https://www.valorantfantasyleague.net/playerstats", {
+    waitUntil: "networkidle0",
+  });
   console.log("Page URL:", page.url());
-  await page.waitForSelector(
-    "tr.group:nth-child(2) > td:nth-child(1) > div:nth-child(2) > span:nth-child(2)",
-  );
 
   //get the raw player and team strings from the page
+  console.log("Getting players");
   let rawPlayerStrings = await getRawPlayerStrings(page);
+  console.log("Getting teams");
   let rawTeamStrings = await getRawTeamStrings(page);
-
+  console.log("Getting historical logs");
+  let rawHistoricalLogStrings = await getHistoricalLogStrings(
+    page,
+    rawPlayerStrings.length,
+  );
   //transform the raw strings into an array of player objects
-  let players = transformRawStringsToPlayers(rawPlayerStrings, rawTeamStrings);
+  let players = transformRawStringsToPlayers(
+    rawPlayerStrings,
+    rawTeamStrings,
+    rawHistoricalLogStrings,
+  );
   console.log(players[0]);
   console.log(players.length);
   await browser.close();
